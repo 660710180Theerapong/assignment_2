2eb7b16fe3c1d07cd513855acd10a9ed814d4f3c
Theerapong Poonkwan <poonkwan_t@su.ac.th>
2026-06-04 05:26:38 +0000

    fix_createTodoUsecase.js

--- todolist/backEnd/usecase/createTodoUsecase.js
+++ todolist/backEnd/usecase/createTodoUsecase.js
@@ -1,6 +1,7 @@
-import createTodo from "../repositories/createTodo"
+import { Todo } from "../domains/Todo"
+import { createTodo } from "../repositories/createTodo"
 
-export const createTodoUsecase = async () =>{
-    const data = createTodo()
-    return data
+export const createTodoUsecase = async (data) =>{
+    const todo = new Todo(data)
+    return await createTodo(todo)
 }
