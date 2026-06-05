7e5cf47b20b807b4ca09a7a8b3b4e4912fc9b8e9
Theerapong Poonkwan <poonkwan_t@su.ac.th>
2026-06-05 06:36:13 +0000

    add_getAllTodoHandler.js

---
+++ todolist/backEnd/handler/getAllTodoHandler.js
@@ -0,0 +1,17 @@
+import { getAllTodoUsecase } from "../usecase/getAllTodoUsecase";
+
+export const getAllTodoUsecase = async(req, res) =>{
+    if (req.method === "GET"){
+                try{
+                    const todo = await getAllTodoUsecase()
+                    res.status(200).json({success: true, data: todo})
+        
+                } catch(err) {
+                    console.error("Get Error: ", err)
+                    res.status(400).json({success: false, error: err.message })
+                }
+        
+            } else {
+                res.status(405).json({success:false, error: "Method not allowed"})
+            }
+}
