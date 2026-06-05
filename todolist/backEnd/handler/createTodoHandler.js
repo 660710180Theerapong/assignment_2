998a2ccd75fec9059d7e35c10a2f9d48d80d0b5f
Theerapong Poonkwan <poonkwan_t@su.ac.th>
2026-06-05 03:59:36 +0000

    add_createTodoHandler.js

---
+++ todolist/backEnd/handler/createTodoHandler.js
@@ -0,0 +1,19 @@
+import { createTodoUsecase } from "../usecase/createTodoUsecase";
+
+export const createTodoHandler = async(req, res) =>{
+    if (req.method === "POST"){
+        try{
+            const todo = await createTodoUsecase(req.body)
+            res.status(200).json({success: true, data: todo})
+
+        } catch(err) {
+            console.error("Create Error: ", err)
+            res.status(400).json({success: false, error: err.massage })
+        }
+
+    } else {
+        res.status(405).json({success:false, error: "Method not allowed"})
+    }
+
+
+}
